586ff7b0cb8c48c1cd85bb9b6d584c6b650af3ca
kyp001 <kyp001@yahoo.com.tw>
2018-09-12 17:29:24 +0800

    update 2018_0912

--- all.js
+++ all.js
@@ -13,6 +13,8 @@ var data = {
 // variable - fix
 var spotMaxItem = 3 ; // one page spot number
 var shiftMaxNo = 5;		// max shift button
+var paginationMax = 5 ;
+var sartPage = 0 ;
 
 
 $(document).ready(function() {
@@ -28,6 +30,16 @@ $(document).ready(function() {
 
 	// show trip data
 	console.log(tripList);
+
+	// show spot counter
+	$(".spot_counetr").text("景點筆數 : " + tripList.length );
+	// show spot
+	showSpotData(tripList, 0, tripList.length, spotMaxItem);
+	// show pagibnation
+	sartPage = showPagination(sartPage, 0, Math.ceil(tripList.length/spotMaxItem)
+									 , paginationMax);
+	// init page event
+	pageEventInit(paginationMax) ;
 });
 
 function getTripDat(data, tripList) {
@@ -61,66 +73,98 @@ function getTripDat(data, tripList) {
 }
 
 // show home spot screen
-function	showSpotData(records, startRecord, totalRecord) {
+function	showSpotData(records, startRecord, totalRecord, spotMaxItem) {
 	var spot = $(".spot");
+	var spot_pic = $(".spot_pic");
+	var spot_name = $(".spot_name");
+	var spot_info = $(".spot_info");
+	var spot_cost = $(".spot_cost");
+	var spot_add = $(".spot_add");
+	var spot_time = $(".spot_time");
 
 	for (var i=0 ; i < spotMaxItem ; i++) {
 		if ((startRecord+i) >= totalRecord) {
-			spot[i].css( "display", "none") ;
+			spot[i].style.display = "none" ;
 		}
 		else {
-			spot[i].css( "display", "block") ;
+			spot[i].style.display = "flex" ;
+ 			spot_pic[i].src =  records[i+startRecord].Picture1;
+	 		spot_name[i].innerText = records[i+startRecord].Name;
+	 		spot_info[i].innerText = records[i+startRecord].Description;
+ 			spot_cost[i].innerText = records[i+startRecord].Ticketinfo;
+	 		spot_cost.next()[i].innerText = records[i+startRecord].Zone;
+	 		spot_add[i].innerText = records[i+startRecord].Add;
+	 		spot_time[i].innerText = records[i+startRecord].Opentime;
+	 	}
+	}
+}
 
-			// img
-			var x = spot[i].(:first-child);
-			x.src = records[i+startRecord].Picture1;
-
-			// spot_title
-			x = x.next() ;
-			x.innerHTML = records[i+startRecord].Name ;
-			// spot_discribe
-			x = x.nextElementSibling ;
-			x.innerHTML = records[i+startRecord].Description ;
-			// spot_dollar + address
-			x = x.nextElementSibling ;
-			x.innerHTML = '<i class="fas fa-dollar-sign"></i>' + records[i+startRecord].Ticketinfo 
-										+"  " +'<i class="fas fa-address-card"></i>' + records[i+startRecord].Add ;
-			// spot_locate + opentime
-			x = x.nextElementSibling ;
-			x.innerHTML = '<i class="fas fa-map-marker-alt"></i>' + records[i+startRecord].Zone 
-										+"  " +'<i class="far fa-calendar-alt"></i>' + records[i+startRecord].Opentime ;
-		}		
+// startPage from 0 
+function	showPagination(startPage, indexPage, totalPage, paginationMax) {
+	var pageItem = $(".page-item") ;
+	var currStartPage = 0 ;
+	
+	if (indexPage>=startPage && indexPage<(startPage+paginationMax)) {
+		currStartPage = startPage;
+	}
+	else {
+		currStartPage = indexPage;
 	}
 
-/*
-	var spot = document.getElementsByClassName("one_spot");
-	for(var i=0 ; i < spotMaxItem ; i++) {
-		if ((startRecord+i) >= totalRecord) {
-			spot[i].style.display = "none";
+	pageItem.removeClass("active").addClass("disabled");
+	for(var i=0 ; i<paginationMax ; i++) {
+		var index = i + 1 ;
+
+		if ((startPage+i) < totalPage) {
+			pageItem[index].style.display = "block";
+			pageItem[index].classList.remove("disabled");
+			pageItem[index].children.innerText = (startPage+1).toString();
 		}
 		else {
-			spot[i].style.display = "block";
-			// img
-			var x = spot[i].firstElementChild;
-			x.src = records[i+startRecord].Picture1;
+			pageItem[index].style.display = "none";
+		}
 
-			// spot_title
-			x = x.nextElementSibling ;
-			x.innerHTML = records[i+startRecord].Name ;
-			// spot_discribe
-			x = x.nextElementSibling ;
-			x.innerHTML = records[i+startRecord].Description ;
-			// spot_dollar + address
-			x = x.nextElementSibling ;
-			x.innerHTML = '<i class="fas fa-dollar-sign"></i>' + records[i+startRecord].Ticketinfo 
-										+"  " +'<i class="fas fa-address-card"></i>' + records[i+startRecord].Add ;
-			// spot_locate + opentime
-			x = x.nextElementSibling ;
-			x.innerHTML = '<i class="fas fa-map-marker-alt"></i>' + records[i+startRecord].Zone 
-										+"  " +'<i class="far fa-calendar-alt"></i>' + records[i+startRecord].Opentime ;
+		if ((i+startPage) == indexPage) {
+			pageItem[index].classList.add("active");
 		}
 	}
-*/
+
+	if (startPage != 0) {
+		pageItem[0].classList.remove("disabled");
+	}
+
+	if ((startPage+paginationMax) < (totalPage-1)) {
+		pageItem[paginationMax+1].classList.remove("disabled");
+	}
+
+	return currStartPage;
+}
+
+// page event init 
+function pageEventInit(paginationMax) {
+	var pageItem = $(".page-item") ;
+
+	for(var i=1 ; i<=paginationMax ; i++) {
+		pageItem[i].onclick = changePage;
+	}
+	pageItem[0].onclick = pageDown ;
+	pageItem[paginationMax+1].onclick = pageUp ;
+}
+
+function changePage(event) {
+	var inValue = Number(event.toElement.innerText) ;
+	sartPage = showPagination(sartPage, inValue-1, Math.ceil(tripList.length/spotMaxItem)
+									 , paginationMax);
+}
+
+function pageDown() {
+	sartPage = showPagination(sartPage, sartPage-1, Math.ceil(tripList.length/spotMaxItem)
+									 , paginationMax);
+}
+
+function pageUp() {
+	sartPage = showPagination(sartPage+1, sartPage+paginationMax, Math.ceil(tripList.length/spotMaxItem)
+									 , paginationMax);
 }
 
 // get parameter value 
